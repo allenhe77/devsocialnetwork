6d0f639f592f508651a98417f494201c9f802fee
allenhe77 <allenhe77@gmail.com>
2020-09-03 20:52:07 +0300

    allow user to edit profile,

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-form/CreateProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import EditProfile from "./components/profile-form/EditProfile";
 //redux
 
 import { Provider } from "react-redux";
@@ -44,6 +45,11 @@ const App = () => {
                 exact
                 path="/create-profile"
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
               />
             </Switch>
           </section>
