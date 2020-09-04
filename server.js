4c8e5097ad4e33e3ac673fbdf59d70365946b4c8
allenhe77 <allenhe77@gmail.com>
2020-09-04 16:35:21 +0300

    prepare files for deployment to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 connectDB();
@@ -19,4 +20,13 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+//server static assets in production
+if (process.env.NODE.ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
